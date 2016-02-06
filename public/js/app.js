46a49dc30b9f1821c294c85c5ddf824259db8590
karunakar <akreddy888@gmail.com>
2016-02-06 10:50:01 +0530

    added custom message event

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,8 @@ var socket = io();
 socket.on('connect', function() {
 	console.log('Connected to socket.io server!');
 });
+
+socket.on('message', function(msg) {
+	console.log('New Message: ');
+	console.log(msg.text);
+});
